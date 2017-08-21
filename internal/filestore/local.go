076da0eb1c1f0bee1e1f540bfb7c396928c9c00a
Antti Kupila <akupila@gmail.com>
2017-08-21 21:38:39 +0200

    Add local filestore

---
+++ internal/filestore/local.go
@@ -0,0 +1,97 @@
+package filestore
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Local stores files locally on disk
+type Local struct {
+	UploadDirectory string
+	SourceDirectory string
+	httpServer      *http.Server
+	address         string
+}
+
+// NewLocal creates a local filestore and starts listening on a port assigned
+// by the operating system. The files are stored on local disk on the server.
+// The uploadDir is used for storing uploads, files are moved to the sourceDir
+// when persisted. The directories are created if they don't already exist.
+func NewLocal(uploadDir, sourceDir string) (*Local, error) {
+	if err := os.MkdirAll(uploadDir, 0755); err != nil {
+		return nil, errors.Wrap(err, "could not make upload directory")
+	}
+	if err := os.MkdirAll(sourceDir, 0755); err != nil {
+		return nil, errors.Wrap(err, "could not make source directory")
+	}
+
+	srv := &http.Server{Addr: "127.0.0.1:0"}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		token := strings.TrimPrefix(r.URL.Path, "/")
+		file, err := os.Create(fmt.Sprintf("%s/%s", uploadDir, token))
+		if err != nil {
+			http.Error(w, errors.Wrap(err, "could not create uploaded file").Error(), http.StatusInternalServerError)
+			return
+		}
+		_, err = io.Copy(file, r.Body)
+		if err != nil {
+			http.Error(w, errors.Wrap(err, "could not save uploaded file").Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	addrc := make(chan string)
+	go func() {
+		lis, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			panic(err)
+		}
+		addrc <- lis.Addr().String()
+		close(addrc)
+		_ = srv.Serve(lis)
+	}()
+
+	l := &Local{
+		UploadDirectory: uploadDir,
+		SourceDirectory: sourceDir,
+		httpServer:      srv,
+		address:         <-addrc,
+	}
+
+	return l, nil
+}
+
+// NewUploadURL creates a new upload url that the local filestore will handle.
+func (l *Local) NewUploadURL(name string) (string, error) {
+	return fmt.Sprintf("http://%s/%s", l.address, name), nil
+}
+
+// Persist moves the file from the upload directory to the source directory.
+func (l *Local) Persist(ctx context.Context, name string) error {
+	from := fmt.Sprintf("%s/%s", l.UploadDirectory, name)
+	to := fmt.Sprintf("%s/%s", l.SourceDirectory, name)
+	if err := os.Rename(from, to); err != nil {
+		return errors.Wrap(err, "could not move to source directory")
+	}
+	return nil
+}
+
+// Shutdown gracefully closes the local filestore. New connections are not
+// accepted after Close() and existing connections are drained before shutdown.
+func (l *Local) Shutdown() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := l.httpServer.Shutdown(ctx); err != nil {
+		return errors.Wrap(err, "failed to gracefully shut down server")
+	}
+	return nil
+}
---
+++ internal/filestore/local_test.go
@@ -0,0 +1,60 @@
+package filestore
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLocal(t *testing.T) {
+	base, err := ioutil.TempDir("", "fragments-test")
+	require.NoError(t, err)
+	uploads := filepath.Join(base, "uploads")
+	source := filepath.Join(base, "source")
+
+	// Invalid arguments
+	_, err = NewLocal("", source)
+	require.Error(t, err)
+	_, err = NewLocal(uploads, "")
+	require.Error(t, err)
+
+	// Create local filestore
+	local, err := NewLocal(uploads, source)
+	require.NoError(t, err)
+
+	// Generate url
+	url, err := local.NewUploadURL("test")
+	require.NoError(t, err)
+
+	// Upload to url
+	fixture, err := ioutil.ReadFile("_testdata/upload.txt")
+	require.NoError(t, err)
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(fixture))
+	require.NoError(t, err)
+	_, err = http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	// Upload should be set
+	_, err = os.Stat(filepath.Join(uploads, "test"))
+	require.NoError(t, err)
+
+	// Persist
+	err = local.Persist(context.Background(), "test")
+	require.NoError(t, err)
+
+	// Assert
+	actual, err := ioutil.ReadFile(filepath.Join(source, "test"))
+	require.NoError(t, err)
+
+	assert.EqualValues(t, string(fixture), string(actual))
+
+	err = local.Shutdown()
+	require.NoError(t, err)
+}
